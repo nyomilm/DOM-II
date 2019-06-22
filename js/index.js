ba0fd5668bedd2f4c7d864f1c8b2f829b1932dae
nyomilm <nyomilm@hotmail.com>
2019-06-22 12:25:58 -0500

    second update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const selectedNav = document.querySelector("header nav");
+
+selectedNav.addEventListener('click', event =>{
+    console.log('Clicked', event.target.textContent, event.screenX, event.screenY)
+
+    const title = document.querySelector('h2')
+    title.textContent = event.target.textContent
+})
+
