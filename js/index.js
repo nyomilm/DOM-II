c71a7fae40b3994ed4e6a3a34c34213486b3b562
nyomilm <nyomilm@hotmail.com>
2019-06-22 14:18:56 -0500

    third update

--- js/index.js
+++ js/index.js
@@ -6,5 +6,29 @@ selectedNav.addEventListener('click', event =>{
 
     const title = document.querySelector('h2')
     title.textContent = event.target.textContent
+    
 })
+
+newFunction();
+
+    
+
+function newFunction() {
+    let button = document.querySelectorAll(".btn");
+    button.onmouseover = function (event) {
+        let target = event.target;
+        target.style.background = 'orange';
+    };
+    button.onmouseout = function (event) {
+        let target = event.target;
+        target.style.background = '';
+    };
+}
+
+let button = document.querySelectorAll(".btn");
+button.addEventListener("mousedown", event => {
+    if(event.button ==0){
+        console.log("Left button");
+    }
+});
 
