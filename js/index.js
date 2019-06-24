2eb0a702b20f4fdf2d83f18040fc6a31b63be1bc
nyomilm <nyomilm@hotmail.com>
2019-06-23 22:49:43 -0500

    another commit

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ newFunction();
     
 
 function newFunction() {
-    let button = document.querySelectorAll(".btn");
+    let button = document.querySelector(".btn");
     button.onmouseover = function (event) {
         let target = event.target;
         target.style.background = 'orange';
@@ -25,10 +25,38 @@ function newFunction() {
     };
 }
 
-let button = document.querySelectorAll(".btn");
+let button = document.querySelector(".btn");
 button.addEventListener("mousedown", event => {
     if(event.button ==0){
         console.log("Left button");
     }
 });
+
+
+//let header = document.querySelector("header");
+document.addEventListener("wheel", function(event){
+    console.log(event.deltaX, event.deltaY);
+})
+
+function dragstart_handler(ev){
+    var img = new Image();
+    img.src = 'adventure.jpg';
+    ev.dataTransfer.setDragImage(img, 10, 10);
+}
+
+
+//$(function(){
+ //   $(document).on('keydown', function(){
+  //      console.log('keydown');
+ //   });
+//});
+
+//let arrowDown = newFunction();
+//function newFunction() {
+//    return document.addEventListener('keydown', function (event) {
+//        if (event.code == 'ArrowDown' && (event.ctrlKey || event.metaKey)) {
+//           alert('Undo!');
+//        }
+//    });
+//}
 
